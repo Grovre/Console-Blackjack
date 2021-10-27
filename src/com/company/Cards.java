3a39df85d42a1b970450b78a5b7f5a34e31423b5
Grovre <50428844+Grovre@users.noreply.github.com>
2021-10-27 18:45:29 -0500

    New basic Cards class methods, new shuffling for cards

--- src/com/company/Cards.java
+++ src/com/company/Cards.java
@@ -1,5 +1,4 @@
 package com.company;
-
 import java.util.Arrays;
 
 public class Cards {
@@ -19,11 +18,11 @@ public class Cards {
     private int[] clovers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
     private int[] hearts = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
     private int[] diamonds = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-
-    private int[] spadesWithJoker = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+    
+    /*private int[] spadesWithJoker = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
     private int[] cloversWithJoker = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
     private int[] heartsWithJoker = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
-    private int[] diamondsWithJoker = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+    private int[] diamondsWithJoker = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};*/
 
     private final String spade = "♠";
     private final String clover = "♣";
@@ -33,10 +32,10 @@ public class Cards {
     public Cards(boolean hasJoker) {
 
         if(hasJoker) {
-            spades = spadesWithJoker;
-            clovers = cloversWithJoker;
-            hearts = heartsWithJoker;
-            diamonds = diamondsWithJoker;
+            spades = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+            clovers = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+            hearts = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+            diamonds = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
         }
 
     }
@@ -48,17 +47,59 @@ public class Cards {
                 ", clovers=" + Arrays.toString(clovers) +
                 ", hearts=" + Arrays.toString(hearts) +
                 ", diamonds=" + Arrays.toString(diamonds) +
-                ", spadesWithJoker=" + Arrays.toString(spadesWithJoker) +
-                ", cloversWithJoker=" + Arrays.toString(cloversWithJoker) +
-                ", heartsWithJoker=" + Arrays.toString(heartsWithJoker) +
-                ", diamondsWithJoker=" + Arrays.toString(diamondsWithJoker) +
-                ", spade='" + spade + '\'' +
-                ", clover='" + clover + '\'' +
-                ", heart='" + heart + '\'' +
-                ", diamond='" + diamond + '\'' +
                 '}';
     }
 
+    public void shuffleAll() {
+        spades = shuffle(spades);
+        clovers = shuffle(clovers);
+        hearts = shuffle(hearts);
+        diamonds = shuffle(diamonds);
+    }
 
+    public int[] shuffle(int[] suit) {
+        int[] temp = new int[suit.length]; // Temporary array for shuffling
+        for(int i = 0; i < suit.length; i++) { // Loops through all spades 1-13 (or 1-14 if joker) to put it into a random index in the temp array
+            int pushIndex = (int) (Math.random() * suit.length); // turns pushIndex into a random number 1-13 (or 14)
+            if(temp[pushIndex] != 0) { // Checks if temp array index is empty
+                i--;
+                continue;
+            }
+            temp[pushIndex] = i+1; // Pushes i into temp at pushIndex
+        }
+        return temp;
+    }
+
+    public int[] getSpades() {
+        return spades;
+    }
+
+    public int[] getHearts() {
+        return hearts;
+    }
+
+    public int[] getDiamonds() {
+        return diamonds;
+    }
+
+    public int[] getClovers() {
+        return clovers;
+    }
+
+    public void setSpades(int[] newSuit) {
+        spades = newSuit;
+    }
+
+    public void setDiamonds(int[] newSuit) {
+        diamonds = newSuit;
+    }
+
+    public void setHearts(int[] newSuit) {
+        hearts = newSuit;
+    }
+
+    public void setClovers(int[] newSuit) {
+        clovers = newSuit;
+    }
 
 }
---
+++ src/com/company/test.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class test {
+    public static void main(String[] args) {
+
+        Cards deck = new Cards(true);
+        System.out.println("Before shuffle:");
+        System.out.println(deck);
+        long start = System.currentTimeMillis();
+        deck.shuffleAll();
+        long end = System.currentTimeMillis();
+        System.out.println("After shuffle:");
+        System.out.println(deck);
+        System.out.println("Shuffle time in ms: " + (end - start));
+
+    }
+}
