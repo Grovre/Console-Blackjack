ea4f9832274e201e1d60b8a93acc3f91cfc48f31
Grovre <50428844+Grovre@users.noreply.github.com>
2021-10-27 20:18:00 -0500

    Update Blackjack.java

--- src/com/company/Blackjack.java
+++ src/com/company/Blackjack.java
@@ -7,14 +7,10 @@ public class Blackjack {
     // private Scanner input = new Scanner(System.in);
     private Cards deck = new Cards(false);
 
-
-
     public Blackjack() {
-        System.out.println("brush");
-    }
-
-    public void showHand() {
-
+        deck.dealCard();
+        deck.dealCard();
+        
     }
 
 }
