5832bbb1386f75b1e098ef995cedbf049c516a43
Grovre <50428844+Grovre@users.noreply.github.com>
2021-11-05 04:29:43 -0500

    recycling

--- src/com/company/test.java
+++ src/com/company/test.java
@@ -3,11 +3,6 @@ package com.company;
 public class test {
     public static void main(String[] args) {
 
-        Deck deck = new Deck();
-        deck.cleanToString();
-        deck.shuffle();
-        deck.cleanToString();
-
         Blackjack bj = new Blackjack();
         bj.progress();
 
