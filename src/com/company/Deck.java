c14b8de16188108ee386e4f8392a22bc3b785fe9
Grovre <50428844+Grovre@users.noreply.github.com>
2021-11-05 04:26:44 -0500

    shuffle duration and proper JQKA string returns

--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -9,16 +9,20 @@ public class Deck {
 
     // Constructor dynamically sets the Card[] deck array an unshuffled deck. Shuffle with shuffle() method
     public Deck() {
+        long start = System.currentTimeMillis();
         for(int suit = 1; suit < 5; suit++) {
             for(int value = 1; value < 14; value++) {
                 deck[(suit-1) * 13 + (value-1)] = new Card(value, suit);
             }
             System.out.println();
         }
+        long end = System.currentTimeMillis();
+        System.out.println("Duration to create deck dynamically: " + (end-start) + " ms");
     }
 
     // Shuffles the deck by taking the first Card object in the original deck and putting it into a random index, then taking the next Card object and doing the same and so on
     public void shuffle() {
+        long start = System.currentTimeMillis();
         int testCount = 0;
         Card[] temp = new Card[deck.length];
         for(int i = 0; i < temp.length; i++) {
@@ -32,6 +36,8 @@ public class Deck {
         }
         System.out.println("Tested all indexes " + testCount + " times to shuffle");
         deck = temp;
+        long end = System.currentTimeMillis();
+        System.out.println("It took " + (end-start) + " ms to shuffle");
     }
 
     public Card[] getDeck() {
@@ -54,7 +60,15 @@ public class Deck {
     public void cleanToString() {
         StringBuilder str = new StringBuilder();
         for(Card card : deck) {
-            str.append(card.getValue()).append(card.getCardSuitString()).append(", ");
+            String value;
+            if(card.getValue() == 11) {
+                value = "J";
+            } else if(card.getValue() == 12) {
+                value = "Q";
+            } else if(card.getValue() == 13) {
+                value = "K";
+            }
+            str.append(card.getValue()).append(card.showCardSuit()).append(", ");
         }
         str = new StringBuilder(str.substring(0, str.length() - 2));
         System.out.println(str);
