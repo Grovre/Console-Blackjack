0a34d1c38060e77c276c350bff26dfdb15d6043f
Grovre <50428844+Grovre@users.noreply.github.com>
2021-11-05 04:29:36 -0500

    additional private vars, special boolean statuses

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -10,7 +10,8 @@ public class Player {
     private Card[] hand = new Card[52];
     private int amountOfCardsInHand = 0;
     private int handTotal = 0;
-    private boolean isOut = false;
+    private boolean isBust = false;
+    private boolean hasBlackjack = false;
 
     public Player(String name, int chips) {
         this.name = name;
@@ -21,14 +22,26 @@ public class Player {
         return name;
     }
 
-    // Sets the player boolean isOut to true
-    public void wentOut() {
-        isOut = true;
+    // Sets the player boolean isBust to true
+    public void setOut(boolean isBust) {
+        this.isBust = true;
+    }
+
+    public void setBlackjack(boolean hasBlackjack) {
+        this.hasBlackjack = hasBlackjack;
+    }
+
+    public boolean getBlackjack() {
+        return hasBlackjack;
+    }
+
+    public boolean getOut() {
+        return isBust;
     }
 
     // Returns whether the player is out of the game or not
-    public boolean isOut() {
-        return isOut;
+    public boolean isBust() {
+        return isBust;
     }
 
     // Refreshes handTotal by counting all the cards in the player's hand again
@@ -39,9 +52,24 @@ public class Player {
         }
     }
 
-    // Finds the card at the index and returns the
-    public String getCardInHand(int index) {
-        return hand[index].getValue() + hand[index].getCardSuitString();
+    // Finds the card at the index and returns the string of the card
+    public String showCardAtHandIndex(int index) {
+        int cardValue = hand[index].getValue();
+        String value = "";
+        if(cardValue > 10 || cardValue == 1) {
+            if(cardValue == 11) {
+                value = "J";
+            } else if(cardValue == 12) {
+                value = "Q";
+            } else if(cardValue == 13) {
+                value = "K";
+            } else if(cardValue == 1) {
+                value = "A";
+            }
+        } else {
+            value = Integer.toString(cardValue);
+        }
+        return value + hand[index].showCardSuit();
     }
 
     public void placeBet(int betAmount) {
@@ -56,7 +84,13 @@ public class Player {
     public void addToHand(Card newCard) {
         hand[amountOfCardsInHand] = newCard;
         amountOfCardsInHand++;
-        handTotal += newCard.getValue();
+        if(newCard.getValue() > 10) {
+            handTotal += 10;
+        } else if(newCard.getValue() == 1 && handTotal + 11 <= 21) {
+            handTotal += 11;
+        } else {
+            handTotal += newCard.getValue();
+        }
     }
 
     // Used to remove a card from the hand
@@ -79,16 +113,15 @@ public class Player {
         return hand;
     }
 
-    public void cleanCardsToString() {
+    public String showHandString() {
         StringBuilder str = new StringBuilder();
         for(Card card : hand) {
             if(card == null) {
                 continue;
             }
-            str.append(card.getValue()).append(card.getCardSuitString()).append(", ");
+            str.append(card.getValue()).append(card.showCardSuit()).append(", ");
         }
-        str = new StringBuilder(str.substring(0, str.length() - 2));
-        System.out.println(str);
+        return str.substring(0, str.length() - 2);
     }
 
     @Override
