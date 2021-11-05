29009b1195d8f46552dd2f94012b8418cd79cd7c
Grovre <50428844+Grovre@users.noreply.github.com>
2021-11-05 04:27:39 -0500

    hit or pass mostly completed
    improvements, ace 1 or 11 to come next

--- src/com/company/Blackjack.java
+++ src/com/company/Blackjack.java
@@ -15,35 +15,59 @@ public class Blackjack {
         for(Player player : players) {
             player.addToHand(deck.getTopOfDeck());
             player.addToHand(deck.getTopOfDeck());
-            System.out.println(player.getName() + " received: " + player.getCardInHand(0) + ", " + player.getCardInHand(1));
+            System.out.println(player.getName() + " received: " + player.showCardAtHandIndex(0) + ", " + player.showCardAtHandIndex(1));
         }
 
+    }
+
+    public Deck getDeck() {
+        return deck;
     }
 
     public void progress() {
 
         for(Player player : players) {
-            System.out.println("\n\n");
-            System.out.print(player.getName() + "'s cards: ");
-            player.cleanCardsToString();
-            System.out.println("Total: " + player.getHandTotal());
+            System.out.println("\n");
             if(player.getHandTotal() > 21) {
                 System.out.println(player.getName() + " busted.");
-                player.wentOut();
+                player.setOut(true);
             } else if(player.getHandTotal() == 21) {
                 System.out.println("Blackjack!");
-            } else if(player.getCardInHand(0) == player.getCardInHand(1) && player.getCardInHand(3) == null){
+                player.setBlackjack(true);
+            } /*else if(player.getCardInHand(0) == player.getCardInHand(1) && player.getCardInHand(3) == null){
+                split(player);
+            }*/
+
+            // Checks if a player has their hasBlackjack status set to true, and if they do then it skips to the next person
+            if(player.getBlackjack()) {
+                continue;
+            }
+
+            boolean ongoingTurn = true;
+            System.out.println("\n\n" + player.getName() + "'s turn");
+            while(ongoingTurn) {
+                System.out.println("Current hand: " + player.showHandString());
+                System.out.println("Hit (1) or pass (2)?");
+                int hitOrPass = input.nextInt();
+                if (hitOrPass == 1) {
+                    player.addToHand(deck.getTopOfDeck());
+                    System.out.println("New card: " + player.showCardAtHandIndex(2));
+                } else if (hitOrPass == 2) {
+                    System.out.println("Passed.");
+                    ongoingTurn = false;
+                }
+
+                if(player.getHandTotal() == 21) {
+                    System.out.println("Blackjack!");
+                    ongoingTurn = false;
+                } else if(player.getHandTotal() > 21) {
+                    System.out.println("Busted.");
+                    ongoingTurn = false;
+                }
 
             }
 
-            /*boolean canSplit = false;
-            for(Card cardToCheck : player.getHand()) {
-                for(Card cardForChecking : player.getHand()) {
-                    if(cardToCheck.getValue() == cardForChecking.getValue()) {
-                        canSplit = true;
-                    }
-                }
-            }*/
+            System.out.println(player.getName() + "'s closing hand: " + player.showHandString());
 
         }
 
